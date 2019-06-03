457a34e1034e23af4b80d287e567a0802c6bcf89
elrohil <wieslaw.stanek@spinney.io>
2019-06-03 08:13:18 +0200

    GraphFrame

--- src/main/scala/pl/edu/agh/Main.scala
+++ src/main/scala/pl/edu/agh/Main.scala
@@ -1,40 +1,23 @@
 package pl.edu.agh
 
-
-import scala.collection.JavaConverters._
-
 object Main extends SparkInit {
   def main(args: Array[String]): Unit = {
-    import org.neo4j.spark.{Neo4jGraph, Neo4jGraphFrame, Neo4j}
-    import org.neo4j.driver.v1.types.{Path, Node, Relationship, Entity}
-    import org.graphframes.GraphFrame
+    import org.neo4j.spark.{Neo4j}
 
     val neo4j = Neo4j(sc = sc)
 
-    val graph = neo4j
-      .cypher("MATCH p = (:Compound)-[*2..4]-(:Disease) RETURN p LIMIT 50")
+    val graphFrame = neo4j
       .partitions(4)
       .batch(50)
-      .loadRowRdd
-      .flatMap(r => {
-        val path = r.getAs[Path](0)
-        Seq(
-          ("nodes", path.nodes().asScala.toArray[Entity]),
-          ("rels", path.relationships().asScala.toArray[Entity])
-        )
-      })
-      .map(d => {
-        val entities = d._1 match {
-          case "nodes" =>
-            d._2.map(e => (e.id(), null.asInstanceOf[Long], null.asInstanceOf[String]))
-          case "rels" =>
-            d._2.map(r => (r.asInstanceOf[org.neo4j.driver.v1.types.Relationship].startNodeId(), r.asInstanceOf[org.neo4j.driver.v1.types.Relationship].endNodeId(), r.asInstanceOf[org.neo4j.driver.v1.types.Relationship].`type`()))
-        }
-        (d._1, entities)
-      })
-      .reduceByKey((a, b) => Array.concat(a, b))
-      .collect()
+      .rels("MATCH (a)-[r]->(b) RETURN ID(a) as src, ID(b) as dst, type(r) as relationship")
+      .nodes("MATCH (n) RETURN ID(n) as id, labels(n)[0] as type", Map.empty)
+      .loadGraphFrame
 
-    println(graph(0), graph(1))
+    val paths = graphFrame
+      .find("(a)-[e]->(b)")
+      .filter("a.type = 'Compound'")
+      .filter("b.type = 'Disease'")
+
+
   }
 }
